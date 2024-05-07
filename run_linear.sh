bdd6812410abd5f6fd64d6cc81224ef0b5dfcf23
hiro <jinguang.tong@anu.edu.au>
2024-05-07 12:34:51 +1000

    add some running script

---
+++ run_linear.sh
@@ -0,0 +1,7 @@
+scenes=("car" "coffee" "helmet" "teapot" "toaster")
+for scene in ${scenes[@]}; do
+    echo "Running scene: $scene"
+    CUDA_VISIBLE_DEVICES='2' python train.py -s data/refnerf/${scene} --eval -m output/${scene}_linear  -w --brdf_dim 0 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512 --port 6002 --linear &&
+    CUDA_VISIBLE_DEVICES='2' python render.py -m output/${scene}_linear --brdf_dim 0 --sh_degree -1 --brdf_mode envmap --brdf_env 512 &&
+    python metrics.py -m output/${scene}_linear
+done
