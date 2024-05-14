6cc9d5f90075b79c680c98bde69a48ffb6d49eb0
hiro <jinguang.tong@anu.edu.au>
2024-05-14 13:51:02 +1000

    format code

--- SIBR_viewers/src/projects/dataset_tools/preprocess/converters/colmap2sibr.py
+++ SIBR_viewers/src/projects/dataset_tools/preprocess/converters/colmap2sibr.py
@@ -1,17 +1,17 @@
 # Copyright (C) 2020, Inria
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
-# 
-# This software is free for non-commercial, research and evaluation use 
+#
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
-# 
+#
 # For inquiries contact sibr@inria.fr and/or George.Drettakis@inria.fr
 
 
 #!/usr/bin/env python
 #! -*- encoding: utf-8 -*-
 
-""" @package dataset_tools_preprocess
+"""@package dataset_tools_preprocess
 This script calls meshlab to simplify a mesh
 
 Parameters: --h help,
@@ -33,13 +33,13 @@ from pathlib import Path
 
 
 def checkColmapConsistent(pathdir):
-    colmapDir = Path(pathdir+"/colmap")
+    colmapDir = Path(pathdir + "/colmap")
 
     if not os.path.isdir(colmapDir):
         return False
 
     # check for mesh
-    colmapmesh = Path(pathdir+"/colmap/stereo/meshed-delaunay.ply")
+    colmapmesh = Path(pathdir + "/colmap/stereo/meshed-delaunay.ply")
     if not os.path.isfile(colmapmesh):
         print("SIBR_ERROR: colmap directory exists but there is no mesh ", colmapmesh)
         print("No file", colmapmesh)
@@ -47,25 +47,32 @@ def checkColmapConsistent(pathdir):
 
     return True
 
+
 def main():
     parser = argparse.ArgumentParser()
 
     # common arguments
-    parser.add_argument("--path", type=str, required=True, help="path to dataset folder")
+    parser.add_argument(
+        "--path", type=str, required=True, help="path to dataset folder"
+    )
 
     args = vars(parser.parse_args())
 
-    if not checkColmapConsistent(args['path']):
-        print("SIBR_ERROR Colmap hasnt been run properly; run it first (ie dont use --noColmap)")
+    if not checkColmapConsistent(args["path"]):
+        print(
+            "SIBR_ERROR Colmap hasnt been run properly; run it first (ie dont use --noColmap)"
+        )
         sys.exit(1)
 
-	# prepareColmap4Sibr: convert cameras and create bundle file put everything in sfm_mvs_cm, then run the normal preprocessing
-    # 
+    # prepareColmap4Sibr: convert cameras and create bundle file put everything in sfm_mvs_cm, then run the normal preprocessing
+    #
     prepareColmap_app = getProcess("prepareColmap4Sibr")
-    
-    prepareColmap_args = [prepareColmap_app,
-        "--path", args['path'],
-        ]
+
+    prepareColmap_args = [
+        prepareColmap_app,
+        "--path",
+        args["path"],
+    ]
 
     print("Running prepareColmap4Sibr ", prepareColmap_args)
     p_exit = subprocess.call(prepareColmap_args)
@@ -73,18 +80,29 @@ def main():
         print("SIBR ERROR: prepareColmap4Sibr failed, exiting")
         sys.exit(1)
 
-	# run rc_to_sibr process to make all images have the same size and be compatible with spixelwarp pipeline
-    p_exit = subprocess.call(["python", "ibr_preprocess_rc_to_sibr.py",  "-i", args['path']+"/sfm_mvs_cm",  "-o", args['path']+"/sibr_cm"])
+    # run rc_to_sibr process to make all images have the same size and be compatible with spixelwarp pipeline
+    p_exit = subprocess.call(
+        [
+            "python",
+            "ibr_preprocess_rc_to_sibr.py",
+            "-i",
+            args["path"] + "/sfm_mvs_cm",
+            "-o",
+            args["path"] + "/sibr_cm",
+        ]
+    )
     if p_exit != 0:
-        print("SIBR_ERROR preprocess to sibr_cm failed");
+        print("SIBR_ERROR preprocess to sibr_cm failed")
         sys.exit(1)
 
     prepareColmap_app = getProcess("prepareColmap4Sibr")
-    
-    prepareColmap_args = [prepareColmap_app,
-		"--fix_metadata", 
-        "--path", args['path'],
-        ]
+
+    prepareColmap_args = [
+        prepareColmap_app,
+        "--fix_metadata",
+        "--path",
+        args["path"],
+    ]
 
     print("Running prepareColmap4Sibr to fix scene_dataset.txt ", prepareColmap_args)
     p_exit = subprocess.call(prepareColmap_args)
@@ -92,8 +110,8 @@ def main():
         print("SIBR ERROR: prepareColmap4Sibr failed, exiting")
         sys.exit(1)
 
-
     sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
--- scene/NVDIFFREC/__init__.py
+++ scene/NVDIFFREC/__init__.py
@@ -1,1 +1,7 @@
-from scene.NVDIFFREC.light import EnvironmentLight, create_trainable_env_rnd, save_env_map, load_env, extract_env_map
+from scene.NVDIFFREC.light import (
+    EnvironmentLight,
+    create_trainable_env_rnd,
+    save_env_map,
+    load_env,
+    extract_env_map,
+)
