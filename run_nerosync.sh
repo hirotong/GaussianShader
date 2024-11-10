65f98dc471ede80024f6f3e551690d14c8986574
Tong, Jinguang (Data61, Black Mountain) <Jinguang.Tong@data61.csiro.au>
2024-11-10 23:32:09 +1100

    update

--- run_nerosync.sh
+++ run_nerosync.sh
@@ -6,9 +6,11 @@ fi
 cases=$(find $rootdir -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
 for case in ${cases[@]}; do
     echo "Running scene: $case"
-    python train.py -s $rootdir/$case --eval -m output/nerosync/${case} -w --brdf_dim 0 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512
-    python render.py -m output/nerosync/${case} --brdf_dim 0 --sh_degree -1 --brdf_mode envmap --brdf_env 512
+    python train.py -s $rootdir/$case --eval -m output/GShader/nerosync/${case} -w --brdf_dim 0 --sh_degree -1 --lambda_predicted_normal 2e-1 --brdf_env 512
+    python render.py -m output/GShader/nerosync/${case} --brdf_dim 0 --sh_degree -1 --brdf_mode envmap --brdf_env 512
     # extract mesh
-    gs-extract-mesh -m output/nerosync/${case} -o output/nerosync/${case}/test/ours_30000
-    python metrics.py -m output/nerosync/${case}
+    gs-extract-mesh -m output/GShader/nerosync/${case} -o output/GShader/nerosync/${case}/test/ours_30000
+    python metrics.py -m output/GShader/nerosync/${case}
 done
+
+python summary.py output/GShader/nerosync
--- summary.py
+++ summary.py
@@ -3,11 +3,22 @@ import numpy as np
 import os
 import json
 import csv
+import sys
+
+DEFAULT_MESH_DICT = {
+    "completeness": -1,
+    "accuracy": -1,
+    "chamfer-L1": -1,
+    "f-score": -1,
+    "f-score-15": -1,
+    "f-score-20": -1,
+}
 
 # root = "output/original/TanksAndTemple"
 # root = "output/ablation/TanksAndTemple_residual"
-root = "output/ablation/GlossySynthetic_residual"
+# root = "output/ablation/GlossySynthetic_residual"
 # root = "output/original/GlossySynthetic"
+root = sys.argv[1]
 
 f_names = sorted(glob(os.path.join(root, "*")))
 summary = {}
@@ -18,9 +29,7 @@ for f_name in f_names:
     with open(results_path) as json_file:
         contents = json.load(json_file)
         # method = sorted(contents.keys(), key=lambda method : int(method.split('_')[-1]))[-1]
-        method = sorted(
-            contents.keys(), key=lambda method: float(contents[method]["PSNR"])
-        )[-1]
+        method = sorted(contents.keys(), key=lambda method: float(contents[method]["PSNR"]))[-1]
         try:
             summary["Method"].append(os.path.basename(f_name))
             summary["PSNR"].append(contents[method]["PSNR"])
@@ -31,10 +40,20 @@ for f_name in f_names:
             summary["PSNR"] = [contents[method]["PSNR"]]
             summary["SSIM"] = [contents[method]["SSIM"]]
             summary["LPIPS"] = [contents[method]["LPIPS"]]
+
+    if os.path.exists(os.path.join(f_name, "results_mesh.json")):
+        with open(os.path.join(f_name, "results_mesh.json")) as json_file:
+            contents = json.load(json_file)
+            for key, value in DEFAULT_MESH_DICT.items():
+                summary[key].append(contents[method].get(key, value))
+
 summary["Method"].append("Avg.")
 summary["PSNR"].append(np.mean(summary["PSNR"]))
 summary["SSIM"].append(np.mean(summary["SSIM"]))
 summary["LPIPS"].append(np.mean(summary["LPIPS"]))
+
+for key in DEFAULT_MESH_DICT.keys():
+    summary[key].append(np.mean(summary[key]))
 
 with open(os.path.join(root, "summary.csv"), "w") as file_obj:
     writer_obj = csv.writer(file_obj)
@@ -42,3 +61,5 @@ with open(os.path.join(root, "summary.csv"), "w") as file_obj:
     writer_obj.writerow(["PSNR"] + summary["PSNR"])
     writer_obj.writerow(["SSIM"] + summary["SSIM"])
     writer_obj.writerow(["LPIPS"] + summary["LPIPS"])
+    for key in DEFAULT_MESH_DICT.keys():
+        writer_obj.writerow([key] + summary[key])
